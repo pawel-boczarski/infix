2a20af68efe9ddcc35ab337f7b03ff46c1153b2e
Paweł Boczarski <pawel.boczarski@gmail.com>
2016-03-15 23:27:02 +0100

    Parentheses - eval subexpressions, catch signals to break loops

--- main.c
+++ main.c
@@ -5,6 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <alloca.h>
+#include <signal.h>
+
+
+#define DEBUG_PRINTF
+
+#ifdef DEBUG_PRINTF
+#define debug_printf printf
+#else
+#define debug_printf(s, ...) do { } while ( 0 )
+#endif
 
 struct {
 	struct {
@@ -13,6 +23,18 @@ struct {
 	} *namevalue_pairs;
 	int no;
 } vars;
+
+int signal_requested;
+int loop_exit_requested;
+int in_loop;
+
+sighandler_t old_sighandler;
+
+void sighandler(int sig)
+{
+	signal_requested = 1;
+}
+
 
 void setvar(const char *name, int name_start, int name_len, const char *value, int value_start, int value_len)
 {
@@ -71,7 +93,7 @@ void parse(char *command, int start, int len, int *no_tokens, int **token_starts
 	
 	int sub_started = 0;
 	char last_char='\0';
-	int current_start = 0, i;
+	int current_start = start, i;
 	for(i = start; i < start+len; i++)
 	{
 		char this_char = command[i];
@@ -96,7 +118,7 @@ void parse(char *command, int start, int len, int *no_tokens, int **token_starts
 			(*token_starts)[*no_tokens-1] = current_start;
 			*token_lengths = realloc(*token_lengths, *no_tokens * sizeof(int));
 			(*token_lengths)[*no_tokens-1] = i - current_start;
-			printf("(%d,%d)\n",current_start, i-current_start);
+			debug_printf("(%d,%d)\n",current_start, i-current_start);
 
 			current_start = i;
 		}
@@ -109,14 +131,14 @@ store_char_and_continue:
 	(*token_starts)[*no_tokens-1] = current_start;
 	*token_lengths = realloc(*token_lengths, *no_tokens * sizeof(int));
 	(*token_lengths)[*no_tokens-1] = i - current_start;
-			printf("*(%d,%d)\n",current_start, i-current_start);
+			debug_printf("*(%d,%d)\n",current_start, i-current_start);
 }
 
 void debug_tokens(char *command, int no_tokens, int *token_starts, int *token_lengths)
 {
 	for(int i = 0; i < no_tokens; i++)
 	{
-		printf("token %d: (%d,%d)'", i, token_starts[i], token_lengths[i]);
+		debug_printf("token %d: (%d,%d)'", i, token_starts[i], token_lengths[i]);
 		putsubstr(command, token_starts[i], token_lengths[i]);
 		printf("'\n");
 	}
@@ -126,14 +148,32 @@ char *evaluate(char *command, int start, int len)
 {
 	int no_tokens, *token_starts, *token_lengths;
 	char *ret = NULL;
+	printf("evaluate(%d, %d)\n", start, len);
 	parse(command, start, len, &no_tokens, &token_starts, &token_lengths);
 	debug_tokens(command, no_tokens, token_starts, token_lengths);
+	
+	if(signal_requested)
+	{
+		signal_requested = 0;
+		printf("Interrupt: exit last frame.\n");
+		if(in_loop) loop_exit_requested = 1;
+		return strdup("0");
+	}
+	
 	switch(no_tokens) {
 		case 1:
 		{
-			ret = malloc(1+len);
-			memcpy(ret, command+start, len);
-			ret[len] = '\0';
+			if(command[token_starts[0]] == '(' && command[token_starts[0]+token_lengths[0]-1] == ')')
+			{
+				printf("!!!!! %d %d !!!!!!\n", token_starts[0]+1, token_lengths[0]-2);
+				ret = evaluate(command, token_starts[0]+1, token_lengths[0]-2);
+			}
+			else
+			{
+				ret = malloc(1+len);
+				memcpy(ret, command+start, len);
+				ret[len] = '\0';
+			}
 		}
 		break;
 		case 2:
@@ -152,30 +192,42 @@ char *evaluate(char *command, int start, int len)
 				setvar(command, token_starts[2], token_lengths[2],
 				       command, token_starts[0], token_lengths[0]);
 				       
-				ret = malloc(1+token_lengths[1]);
+				ret = malloc(1+token_lengths[0]);
 				memcpy(ret, command+token_starts[0], token_lengths[0]);
 				ret[token_lengths[0]] = '\0';
 			}
 			else
 			if(token_lengths[1]==1 && command[token_starts[1]] == '?')
 			{
+				in_loop = 1;
 				for(;;)
 				{
 					char *le = evaluate(command, token_starts[0], token_lengths[0]);
 					int true_val = atoi(le);
 					free(ret);
 					free(le);
-
-					if(true_val)
-					{
-						ret = evaluate(command, token_starts[2], token_lengths[2]);
-					}
-					else
+					
+					if(!true_val || loop_exit_requested)
 					{
 						ret = strdup("");
 						break;
 					}
+					else
+					{
+						ret = evaluate(command, token_starts[2], token_lengths[2]);
+					}
+
 				}
+				in_loop = 0;
+				loop_exit_requested = 0;
+			}
+			else
+			if(token_lengths[1]==1 && command[token_starts[1]] == ',')
+			{
+				char *le = evaluate(command, token_starts[0], token_lengths[0]);
+				free(le);
+				
+				ret = evaluate(command, token_starts[2], token_lengths[2]);
 			}
 		}
 		break;
@@ -186,6 +238,9 @@ char *evaluate(char *command, int start, int len)
 		break;
 	}
 	
+cleanup:
+	free(token_starts);
+	free(token_lengths);
 	return ret ? ret : calloc(1,1);
 }
 
@@ -193,13 +248,16 @@ int main()
 {
 	char command[4096];
 	
-	while(1)
+	
+	while(!signal_requested)
 	{
 		printf(">> ");
 		fgets(command, 4096, stdin);
 		if(command != strtok(command, "\r\n")) *command = 0;
-		printf("GOT: '%s'\n", command);
+		debug_printf("GOT: '%s'\n", command);
+		old_sighandler = signal(SIGINT, sighandler);
 		char *res = evaluate(command, 0, strlen(command));
+		signal(SIGINT, old_sighandler);
 		printf("<< '%s'\n", res);
 		free(res);
 	}
